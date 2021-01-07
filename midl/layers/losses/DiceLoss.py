fdfae7db4d3fb2b0ef5334d1ec7b99db82867ed6
LamFSangUk <psu9808@gmail.com>
2021-01-08 03:37:10 +0900

    Revised BinaryDiceLoss for binary class segmentation

--- midl/layers/losses/DiceLoss.py
+++ midl/layers/losses/DiceLoss.py
@@ -11,18 +11,26 @@ import torch.nn as nn
 
 
 class BinaryDiceLoss(nn.Module):
-    def __init__(self, smooth=1):
+    def __init__(self, smooth=1e-6):
         super(BinaryDiceLoss, self).__init__()
         self.smooth = smooth
 
     def forward(self, predict, target):
+        """
+
+        :param predict: N x ... probability map
+        :param target: N x ... ground truth binary image
+        :return: Dice coefficient similarity loss
+        """
+
+        # Flatten
         predict = predict.contiguous().view(predict.shape[0], -1)
         target = target.contiguous().view(target.shape[0], -1)
 
-        numerator = 2 * torch.sum(torch.mul(predict, target), dim=1) + self.smooth
-        denominator = torch.sum(torch.add(predict, target), dim=1) + self.smooth
+        numerator = 2 * torch.sum(torch.mul(predict, target), dim=1)
+        denominator = torch.sum(torch.add(predict, target), dim=1)
 
-        loss = 1 - numerator / denominator
+        loss = 1 - numerator / (denominator + self.smooth)
 
         return loss.mean()
 
